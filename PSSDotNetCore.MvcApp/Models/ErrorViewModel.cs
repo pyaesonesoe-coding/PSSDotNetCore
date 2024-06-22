7aede26484e29fba6fbd415d5c232c4e8e94f888
PyaeSoneSoe <pyaesone.ps15@gmail.com>
2024-06-22 20:23:08 +0630

    [Complete] MVC CRUD

---
+++ PSSDotNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSSDotNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
