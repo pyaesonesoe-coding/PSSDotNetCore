fd675c4c07b56884ece9261a07bb7ddd0eadf3f7
PyaeSoneSoe <pyaesone.ps15@gmail.com>
2024-07-14 14:56:26 +0630

    [Complete] CanvasJs

---
+++ PSSDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSSDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult AreaChart()
+        {
+            return View();
+        }
+
+        public IActionResult PyramidChart()
+        {
+            return View();
+        }
+    }
+}
