8aa708a4aef048983c760618a34083e91cbacc93
PyaeSoneSoe <pyaesone.ps15@gmail.com>
2024-07-14 15:25:58 +0630

    [Complete] Console App, ASP.NET Core Text Log

--- PSSDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
+++ PSSDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -4,8 +4,16 @@ namespace PSSDotNetCore.MvcChartApp.Controllers
 {
     public class CanvasJsController : Controller
     {
-        public IActionResult AreaChart()
+        private readonly ILogger<CanvasJsController> _logger;
+
+		public CanvasJsController(ILogger<CanvasJsController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult AreaChart()
         {
+            _logger.LogInformation("Area Chart...");
             return View();
         }
 
