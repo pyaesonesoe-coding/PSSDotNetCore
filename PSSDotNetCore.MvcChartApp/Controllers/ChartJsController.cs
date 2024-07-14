f72b3c3ae3d5c3c7fb8f0d288d1708549bb7ba7a
PyaeSoneSoe <pyaesone.ps15@gmail.com>
2024-07-14 14:08:33 +0630

    [Complete] HighCharts

---
+++ PSSDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSSDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PSSDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PSSDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+
+        public IActionResult Variwide()
+        {
+            return View();
+        }
+    }
+}
